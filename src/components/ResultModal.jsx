5cd5f8672db9a2d77fab0494905e0b6c1f5d730f
Sudhanshu kumar <Sudhanshuk384@gmail.com>
2024-02-12 15:54:41 +0530

    putting component directly inside html body using createPortal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,5 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
-
+import {createPortal} from 'react-dom';
 const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
     const dialog = useRef();
 
@@ -14,7 +14,7 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
             }
         }
     })
-    return <dialog ref={dialog} className="result-modal" onClose={onReset}>
+    return createPortal(<dialog ref={dialog} className="result-modal" onClose={onReset}>
         {userLost && <h2>You Lost</h2>}
         {!userLost && <h2>Your Score :{score}</h2>}
         <p>The target time was <strong>{targetTime} seconds.</strong> </p>
@@ -22,8 +22,9 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
         <form method="dialog" onSubmit={onReset}>
             <button>Close</button>
         </form>
-    </dialog>
-
+    </dialog>,
+    document.getElementById('modal')
+    )
 });
 
 export default ResultModal;
